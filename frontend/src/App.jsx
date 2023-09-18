ca1875b91b58105a8cef32d0629699d0fefe0a35
ala233 <lyzstella527@gmail.com>
2023-09-18 21:39:51 +1000

    add profile components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom'
+import Profile from './components/Profile/Profile'
 function App() {
   return (
-    <>
-     Hello World
-    </>
+    <Router>
+      <Routes>
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </Router>
   )
 }
 
 export default App
+
+//npm i styled-components react-router-dom
